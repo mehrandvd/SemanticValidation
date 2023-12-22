c0520f6cbd795ed381333bca370fbc760ccb5f9c
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-23 02:16:11 +0330

    Initialize the project

---
+++ src/SemanticValidation/Models/SemanticValidationResult.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace SemanticValidation.Models
+{
+    public class SemanticValidationResult
+    {
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+        [JsonPropertyName("message")]
+        public string? Message { get; set; }
+    }
+}
