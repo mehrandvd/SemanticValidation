b9b02eb992b41da4e547b62b7c717efff13e7579
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-26 03:53:55 +0330

    Add non-async methods

--- src/SemanticValidation/Semantic/Semantic_AreSimilar.cs
+++ src/SemanticValidation/Semantic/Semantic_AreSimilar.cs
@@ -19,7 +19,7 @@ public partial class Semantic
     /// <param name="first"></param>
     /// <param name="second"></param>
     /// <returns></returns>
-    /// <exception cref="InvalidOperationException"></exception>
+    /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
     public async Task<SemanticValidationResult> AreSimilarAsync(string first, string second)
     {
         var skResult = (
@@ -36,5 +36,24 @@ public partial class Semantic
             throw new InvalidOperationException("Can not assert Similarity");
 
         return result;
+    }
+
+    /// <summary>
+    /// Checks whether <paramref name="first"/> and <paramref name="second"/> string are semantically similar.
+    /// It uses the kernel and OpenAI to check this semantically.
+    /// <example>
+    /// <code>
+    /// AreSimilarAsync("This automobile is red", "The car is red") // returns true
+    /// AreSimilarAsync("This tree is red", "The car is red") // returns false
+    /// </code>
+    /// </example>
+    /// </summary>
+    /// <param name="first"></param>
+    /// <param name="second"></param>
+    /// <returns></returns>
+    /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+    public SemanticValidationResult AreSimilar(string first, string second)
+    {
+        return AreSimilarAsync(first, second).GetAwaiter().GetResult();
     }
 }
--- src/SemanticValidation/Semantic/Semantic_HasCondition.cs
+++ src/SemanticValidation/Semantic/Semantic_HasCondition.cs
@@ -16,10 +16,8 @@ namespace SemanticValidation
         /// </code>
         /// </example>
         /// </summary>
-
         /// <param name="text">The text that the condition should be checked on</param>
         /// <param name="condition">The condition</param>
-
         /// <returns></returns>
         /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
         public async Task<SemanticValidationResult> HasConditionAsync(string text, string condition)
@@ -38,6 +36,25 @@ namespace SemanticValidation
                 throw new InvalidOperationException("Can not assert Similarity");
 
             return result;
+        }
+
+        /// <summary>
+        /// Checks whether <paramref name="text"/> has the <paramref name="condition"/> semantically.
+        /// It uses the kernel and OpenAI to check this semantically.
+        /// <example>
+        /// <code>
+        /// HasConditionAsync("This car is red", "I talks about cars") // returns true
+        /// HasConditionAsync("This car is red", "I talks about trees") // returns false
+        /// </code>
+        /// </example>
+        /// </summary>
+        /// <param name="text">The text that the condition should be checked on</param>
+        /// <param name="condition">The condition</param>
+        /// <returns></returns>
+        /// <exception cref="InvalidOperationException">If the OpenAI was unable to generate a valid response.</exception>
+        public SemanticValidationResult HasCondition(string text, string condition)
+        {
+            return HasConditionAsync(text, condition).GetAwaiter().GetResult();
         }
     }
 }
