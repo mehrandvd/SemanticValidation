ee93a170dc77a53b28cf59b806b55e9db2d972c0
Mehran Davoudi <mehran.davoudi@hotmail.com>
2023-12-28 23:07:04 +0330

    Add PowerParseJson tests.

---
+++ src/SemanticValidation.Tests/PoweParseJsonTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+using SemanticValidation.Utils;
+
+namespace SemanticValidation.Tests
+{
+    public class PowerParseJsonTests
+    {
+        [Fact]
+        public void ParseJson_SimpleObject()
+        {
+            var text = """
+                {
+                    "name": "mehran",
+                    "desc": "software"
+                }
+                """;
+
+            var json = SemanticUtils.PowerParseJson<JsonObject>(text);
+
+            Assert.NotNull(json);
+        }
+
+        [Fact]
+        public void ParseJson_SimpleArray()
+        {
+            var text = """
+                [
+                    {
+                        "name": "mehran"
+                    }  
+                ]
+                """;
+
+            var json = SemanticUtils.PowerParseJson<JsonArray>(text);
+
+            Assert.NotNull(json);
+        }
+
+        [Theory]
+        [InlineData("\"")]
+        [InlineData("'")]
+        [InlineData("`")]
+        [InlineData("```")]
+        public void ParseJson_Quotes(string quote)
+        {
+            var text = $$"""
+                {{quote}}
+                {
+                    "name": "mehran",
+                    "desc": "software"
+                }
+                {{quote}}
+                """;
+
+            var json = SemanticUtils.PowerParseJson<JsonObject>(text);
+
+            Assert.NotNull(json);
+        }
+
+        [Fact]
+        public void ParseJson_JsonPrefix()
+        {
+            var text = """
+                ```json
+                {
+                    "name": "mehran",
+                    "desc": "software"
+                }
+                ```
+                """;
+
+            var json = SemanticUtils.PowerParseJson<JsonObject>(text);
+
+            Assert.NotNull(json);
+        }
+
+        [Fact]
+        public void ParseJson_DoubleQuote()
+        {
+            var text = """
+                "
+                {
+                    "name": "mehran",
+                    "desc": "software"
+                }
+                ```
+                """;
+
+            var json = SemanticUtils.PowerParseJson<JsonObject>(text);
+
+            Assert.NotNull(json);
+        }
+
+    }
+}
